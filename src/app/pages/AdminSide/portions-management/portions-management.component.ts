57c57bd72f885d571bc0db514fffd6981344410d
Patrick Melo <patrickabimael@yahoo.com.br>
2023-09-22 12:12:01 -0300

    implements "back" button for portion

--- src/app/pages/AdminSide/portions-management/portions-management.component.ts
+++ src/app/pages/AdminSide/portions-management/portions-management.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-portions-management',
   templateUrl: './portions-management.component.html',
   styleUrls: ['./portions-management.component.css']
 })
-export class PortionsManagementComponent {
+export class PortionsManagementComponent implements OnInit{
 
+  constructor(
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    sessionStorage.removeItem('redirectingTo')
+  }
+
+  goToAdminPage(): void {
+    this.router.navigate(['/admin'])
+  }
 }
