1a74c9bd4cf9b677e5a5afe0ead2b768754bacfe
Patrick Melo <patrickabimael@yahoo.com.br>
2023-09-24 11:25:19 -0300

    implements combo service

---
+++ src/app/services/ComboService.ts
@@ -0,0 +1,49 @@
+import {BehaviorSubject, Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {Combo} from "../models/Combo";
+import {environment} from "../../environment/environment";
+import {ComboDTO} from "../types/ComboDTO";
+import {ResponseMessage} from "../types/ResponseMessage";
+import {defaultWithToken, withTokenAndBody} from "../types/Headers";
+import {Injectable} from "@angular/core";
+
+@Injectable({
+  providedIn: "root"
+})
+export class DrinkService {
+  private combosSource = new BehaviorSubject<Combo[]>([])
+  currentCombos = this.combosSource.asObservable()
+
+  constructor(private http: HttpClient) {}
+
+  getCombos(): Observable<Combo> {
+    this.http.get<ComboDTO[]>(`${environment.API_URL}/get-all-combos`, {headers: defaultWithToken})
+      .subscribe(combos => {
+        const allCombos = combos.map(combo => new Combo(combo))
+        this.combosSource.next(allCombos)
+      })
+    return this.currentCombos
+  }
+  saveCombo(comboDTO: ComboDTO) {
+    this.http.post<ResponseMessage>(`${environment.API_URL}/add-combo`, comboDTO, {headers: withTokenAndBody})
+      .subscribe(message => {
+        console.info(message)
+        this.getCombos()
+      })
+  }
+  removeCombo(comboIdentifier: string){
+    this.http.delete<ResponseMessage>(`${environment.API_URL}/remove-combo/${comboIdentifier}`, {headers: defaultWithToken})
+      .subscribe(message => {
+        console.info(message)
+        this.getCombos()
+      })
+  }
+  updateCombo(comboDTO: ComboDTO) {
+    this.http.put<ResponseMessage>(`${environment.API_URL}/update-combo`, comboDTO,{headers: withTokenAndBody})
+      .subscribe(message => {
+        console.info(message)
+        this.getCombos()
+      })
+  }
+
+}
---
+++ src/app/types/ComboDTO.ts
@@ -0,0 +1,7 @@
+export interface ComboDTO {
+  identifier: string
+  title: string
+  price: number
+  description: string
+  inStock: boolean;
+}
