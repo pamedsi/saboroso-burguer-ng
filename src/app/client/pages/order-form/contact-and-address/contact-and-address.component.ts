72f290b610595af36d1e7505bf525187f4d94c30
Patrick Melo <patrickabimael@yahoo.com.br>
2023-10-11 15:07:23 -0300

    creates field for address

--- src/app/client/pages/order-form/contact-and-address/contact-and-address.component.ts
+++ src/app/client/pages/order-form/contact-and-address/contact-and-address.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {OrderService} from "../../../services/OrderService";
+import {ClientOrder} from "../../../models/ClientOrder";
 
 @Component({
   selector: 'app-contact-and-address',
   templateUrl: './contact-and-address.component.html',
   styleUrls: ['./contact-and-address.component.css']
 })
-export class ContactAndAddressComponent {
+export class ContactAndAddressComponent implements OnInit{
   ddd!: string;
   phoneNumber!: string;
+  address!: string
+  order!: ClientOrder
+
+  constructor(private orderService: OrderService) {
+  }
+
+  ngOnInit() {
+    this.orderService.currentOrder.subscribe(order => this.order = order)
+  }
 
   numberOnly(pressedKey: KeyboardEvent, id: string) {
     const key = pressedKey.key;
@@ -27,6 +38,10 @@ export class ContactAndAddressComponent {
     let phoneNumber = this.phoneNumber;
     if (phoneNumber.length === 5) phoneNumber = phoneNumber.slice(0, 5) + '-' + phoneNumber.slice(5);
     this.phoneNumber = phoneNumber;
+  }
+
+  confirmOrder(){
+
   }
 
 }
