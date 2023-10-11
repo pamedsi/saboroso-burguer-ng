b87f1393384f2aa1b9d2d44dd376c85c8ca3e874
Patrick Melo <patrickabimael@yahoo.com.br>
2023-10-11 14:02:16 -0300

    implements phone number input field

--- src/app/client/pages/order-form/contact-and-address/contact-and-address.component.ts
+++ src/app/client/pages/order-form/contact-and-address/contact-and-address.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-and-address.component.css']
 })
 export class ContactAndAddressComponent {
+  ddd!: string;
+  phoneNumber!: string;
+
+  numberOnly(pressedKey: KeyboardEvent, id: string) {
+    const key = pressedKey.key;
+    if (key !== 'Backspace' && isNaN(Number(key))) {
+      pressedKey.preventDefault();
+    }
+
+    document.getElementById('ddd')!.addEventListener('keyup', (event) => {
+      const input = event.target as HTMLInputElement;
+      if (input.value.length >= 2) {
+        document.getElementById('phone-number')!.focus();
+      }
+    });
+  }
+
+  formatPhoneNumber() {
+    let phoneNumber = this.phoneNumber;
+    if (phoneNumber.length === 5) phoneNumber = phoneNumber.slice(0, 5) + '-' + phoneNumber.slice(5);
+    this.phoneNumber = phoneNumber;
+  }
 
 }
