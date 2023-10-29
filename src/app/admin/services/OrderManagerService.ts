ccbaaa8c1c1d3395b783774e717778f73e7cbc64
Patrick Melo <patrickabimael@yahoo.com.br>
2023-10-28 22:23:07 -0300

    funcionaliza envio e recebimento de mensagens

---
+++ src/app/admin/services/OrderManagerService.ts
@@ -0,0 +1,18 @@
+// import { webSocket } from 'rxjs/webSocket';
+console.log("oieee")
+
+// export class OrderService {
+//
+//   private socket$ = webSocket('ws://localhost:8080/websocket-demo');
+//
+//   constructor() {
+//     this.socket$.subscribe(
+//       msg => {
+//         console.log('Pedido recebido: ', msg);
+//       },
+//       err => console.error(err),
+//       () => console.log('Conexão encerrada')
+//     );
+//   }
+//
+// }
