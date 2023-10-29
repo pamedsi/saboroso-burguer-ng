c29d2d92fe76f4a02a8953ea4d94a4e5d44c9bff
Patrick Melo <patrickabimael@yahoo.com.br>
2023-10-29 14:16:43 -0300

    creates order DTO for order manager

--- src/app/admin/services/OrderManagerService.ts
+++ src/app/admin/services/OrderManagerService.ts
@@ -4,6 +4,7 @@ import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environment/environment";
 import {defaultWithToken} from "../utils/Headers";
 import {WebSocketService} from "./WebSocketService";
+import {OrderResponseDTO} from "../models/orderDTO/OrderResponseDTO";
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class OrderManagerService {
   constructor(private http: HttpClient, private webSocketService: WebSocketService) {}
 
   getAllOrders(){
-    return this.http.get(`${environment.API_URL}/order-manager`, {headers: defaultWithToken})
+    return this.http.get<OrderResponseDTO[]>(`${environment.API_URL}/order-manager`, {headers: defaultWithToken})
   }
 
 }
